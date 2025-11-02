da2d66c21486148cef8953ef95e76174e4b1c916
MOHAMED-REDA-BOUATAOUI <bouataoui09@gmail.com>
2025-11-02 19:19:03 +0100

    Initial commit

---
+++ src/main/java/dao/DaoImpl2.java
@@ -0,0 +1,11 @@
+package dao;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Primary;
+
+@Primary
+@Component("dao2")
+@Profile("dev")
+public class DaoImpl2 implements IDao {
+    @Override public double getValue(){ return 150.0; }
+}
---
+++ src/main/java/metier/MetierImpl.java
@@ -0,0 +1,13 @@
+package metier;
+
+import dao.IDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("metier")
+public class MetierImpl implements IMetier {
+    @Autowired
+    private IDao dao;                 // pas de @Qualifier ici
+    @Override
+    public double calcul() { return dao.getValue() * 2; }
+}
